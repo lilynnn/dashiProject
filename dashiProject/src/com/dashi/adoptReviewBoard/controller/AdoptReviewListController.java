2bc3ee01b2322632bb8de04fc25860dfd90b73be
EunsuhJeon <87908418+EunsuhJeon@users.noreply.github.com>
2021-12-14 23:47:18 +0900

    입양후기 url주소 수정 -은서

--- dashiProject/src/com/dashi/adoptReviewBoard/controller/AdoptReviewListController.java
+++ dashiProject/src/com/dashi/adoptReviewBoard/controller/AdoptReviewListController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AdoptReviewListController
  */
-@WebServlet("/reviewlist.bo")
+@WebServlet("/list.rv")
 public class AdoptReviewListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
