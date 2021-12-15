f399905fa5f773592a9a0edb1f4b752f55f6c586
jihee <oosoojh31@gmail.com>
2021-12-15 16:38:41 +0900

    관리자메인페이지 url수정-지희

--- dashiProject/WebContent/views/admin/adminMenubar.jsp
+++ dashiProject/WebContent/views/admin/adminMenubar.jsp
@@ -86,7 +86,7 @@
                 <div class="aMenu" id="list">게시글관리</div>
                 <ul>
                     <li><a href="<%=request.getContextPath()%>/noList.ad?cpage=1" class="menu-detail">공지사항</a></li>
-                    <li><a href="<%=request.getContextPath()%>/adlist.ad" class="menu-detail">입양공고</a></li>
+                    <li><a href="<%=request.getContextPath()%>/adlist.ad?cpage=1" class="menu-detail">입양공고</a></li>
                     <li><a href="<%=request.getContextPath()%>/reviewlist.ad" class="menu-detail">입양후기</a></li>
                     <li><a href="<%=request.getContextPath()%>/adlist.dsp" class="menu-detail">실종/보호</a></li>
                     <li><a href="" class="menu-detail">1:1문의</a></li>
--- dashiProject/src/com/dashi/adoptBoard/controller/AdminAdoptListController.java
+++ dashiProject/src/com/dashi/adoptBoard/controller/AdminAdoptListController.java
@@ -1,11 +1,14 @@
 package com.dashi.adoptBoard.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.dashi.adoptBoard.model.service.AdoptBoardService;
 
 /**
  * Servlet implementation class AdminAdoptListController
@@ -26,6 +29,25 @@ public class AdminAdoptListController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int listCount;
+		int currentPage;
+		int pageLimit;
+		int boardLimit;
+		
+		int maxPage;
+		int startPage;
+		int endPage;
+		
+		listCount = new AdoptBoardService().selectListCount();
+		
+		currentPage = Integer.parseInt(request.getParameter("cpage"));
+		
+		
+		
+		
+		
+		
 		
 		
 		
