90bc73584218bc953eeb6a8da5de5ae9595e6f0f
lilynnn <pinksoltlatte@gmail.com>
2021-12-15 13:34:25 +0900

    입소,1:1 매개변수 생성

--- dashiProject/src/com/dashi/answerBoard/model/vo/Answer.java
+++ dashiProject/src/com/dashi/answerBoard/model/vo/Answer.java
@@ -2,22 +2,135 @@ package com.dashi.answerBoard.model.vo;
 
 import java.sql.Date;
 
-	public class Answer {
-	
-		private String inquireNo;
-		private int memNo;
-		private String memId;
-		private String qTitle;
-		private String qContent;
-		private String anContent;
-		private Date qCreat;
-		private Date inquireAnswerDate;
-		private int mnNo;
+public class Answer {
+
+	private String inquireNo;
+	private int memNo;
+	private String memId;
+	private String qTitle;
+	private String qContent;
+	private String anContent;
+	private Date qCreat;
+	private Date inquireAnswerDate;
+	private int mnNo;
 
 
-		public Answer() {}
+	public Answer() {}
 
 
+	public Answer(String inquireNo, int memNo, String memId, String qTitle, String qContent, String anContent,
+			Date qCreat, Date inquireAnswerDate, int mnNo) {
+		super();
+		this.inquireNo = inquireNo;
+		this.memNo = memNo;
+		this.memId = memId;
+		this.qTitle = qTitle;
+		this.qContent = qContent;
+		this.anContent = anContent;
+		this.qCreat = qCreat;
+		this.inquireAnswerDate = inquireAnswerDate;
+		this.mnNo = mnNo;
+	}
+
+
+	public String getInquireNo() {
+		return inquireNo;
+	}
+
+
+	public void setInquireNo(String inquireNo) {
+		this.inquireNo = inquireNo;
+	}
+
+
+	public int getMemNo() {
+		return memNo;
+	}
+
+
+	public void setMemNo(int memNo) {
+		this.memNo = memNo;
+	}
+
+
+	public String getMemId() {
+		return memId;
+	}
+
+
+	public void setMemId(String memId) {
+		this.memId = memId;
+	}
+
+
+	public String getqTitle() {
+		return qTitle;
+	}
+
+
+	public void setqTitle(String qTitle) {
+		this.qTitle = qTitle;
+	}
+
+
+	public String getqContent() {
+		return qContent;
+	}
+
+
+	public void setqContent(String qContent) {
+		this.qContent = qContent;
+	}
+
+
+	public String getAnContent() {
+		return anContent;
+	}
+
+
+	public void setAnContent(String anContent) {
+		this.anContent = anContent;
+	}
+
+
+	public Date getqCreat() {
+		return qCreat;
+	}
+
+
+	public void setqCreat(Date qCreat) {
+		this.qCreat = qCreat;
+	}
+
+
+	public Date getInquireAnswerDate() {
+		return inquireAnswerDate;
+	}
+
+
+	public void setInquireAnswerDate(Date inquireAnswerDate) {
+		this.inquireAnswerDate = inquireAnswerDate;
+	}
+
+
+	public int getMnNo() {
+		return mnNo;
+	}
+
+
+	public void setMnNo(int mnNo) {
+		this.mnNo = mnNo;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Answer [inquireNo=" + inquireNo + ", memNo=" + memNo + ", memId=" + memId + ", qTitle=" + qTitle
+				+ ", qContent=" + qContent + ", anContent=" + anContent + ", qCreat=" + qCreat + ", inquireAnswerDate="
+				+ inquireAnswerDate + ", mnNo=" + mnNo + "]";
+	}	
+		
+		
 		}
 
 
--- dashiProject/src/com/dashi/entranceBoard/model/vo/Entrance.java
+++ dashiProject/src/com/dashi/entranceBoard/model/vo/Entrance.java
@@ -1,5 +1,7 @@
 package com.dashi.entranceBoard.model.vo;
 
+import java.sql.Date;
+
 public class Entrance {
 	
 	private String entNo;
@@ -23,5 +25,197 @@ public class Entrance {
 	private String animalType;
 	
 	public Entrance() {}
+
+	public Entrance(String entNo, int memNo, String memName, String entTitle, Date entApplyDate, String processResult,
+			String animalVariety, String animalName, String animalGender, int animalAge, String animalVaccinated,
+			String animalNeturalization, String animalDisease, String animalIssue, Date entWantDate, int entWantTime,
+			String reqPhone, Date entDate, String animalType) {
+		super();
+		this.entNo = entNo;
+		this.memNo = memNo;
+		this.memName = memName;
+		this.entTitle = entTitle;
+		this.entApplyDate = entApplyDate;
+		this.processResult = processResult;
+		this.animalVariety = animalVariety;
+		this.animalName = animalName;
+		this.animalGender = animalGender;
+		this.animalAge = animalAge;
+		this.animalVaccinated = animalVaccinated;
+		this.animalNeturalization = animalNeturalization;
+		this.animalDisease = animalDisease;
+		this.animalIssue = animalIssue;
+		this.entWantDate = entWantDate;
+		this.entWantTime = entWantTime;
+		this.reqPhone = reqPhone;
+		this.entDate = entDate;
+		this.animalType = animalType;
+	}
+
+	public String getEntNo() {
+		return entNo;
+	}
+
+	public void setEntNo(String entNo) {
+		this.entNo = entNo;
+	}
+
+	public int getMemNo() {
+		return memNo;
+	}
+
+	public void setMemNo(int memNo) {
+		this.memNo = memNo;
+	}
+
+	public String getMemName() {
+		return memName;
+	}
+
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+
+	public String getEntTitle() {
+		return entTitle;
+	}
+
+	public void setEntTitle(String entTitle) {
+		this.entTitle = entTitle;
+	}
+
+	public Date getEntApplyDate() {
+		return entApplyDate;
+	}
+
+	public void setEntApplyDate(Date entApplyDate) {
+		this.entApplyDate = entApplyDate;
+	}
+
+	public String getProcessResult() {
+		return processResult;
+	}
+
+	public void setProcessResult(String processResult) {
+		this.processResult = processResult;
+	}
+
+	public String getAnimalVariety() {
+		return animalVariety;
+	}
+
+	public void setAnimalVariety(String animalVariety) {
+		this.animalVariety = animalVariety;
+	}
+
+	public String getAnimalName() {
+		return animalName;
+	}
+
+	public void setAnimalName(String animalName) {
+		this.animalName = animalName;
+	}
+
+	public String getAnimalGender() {
+		return animalGender;
+	}
+
+	public void setAnimalGender(String animalGender) {
+		this.animalGender = animalGender;
+	}
+
+	public int getAnimalAge() {
+		return animalAge;
+	}
+
+	public void setAnimalAge(int animalAge) {
+		this.animalAge = animalAge;
+	}
+
+	public String getAnimalVaccinated() {
+		return animalVaccinated;
+	}
+
+	public void setAnimalVaccinated(String animalVaccinated) {
+		this.animalVaccinated = animalVaccinated;
+	}
+
+	public String getAnimalNeturalization() {
+		return animalNeturalization;
+	}
+
+	public void setAnimalNeturalization(String animalNeturalization) {
+		this.animalNeturalization = animalNeturalization;
+	}
+
+	public String getAnimalDisease() {
+		return animalDisease;
+	}
+
+	public void setAnimalDisease(String animalDisease) {
+		this.animalDisease = animalDisease;
+	}
+
+	public String getAnimalIssue() {
+		return animalIssue;
+	}
+
+	public void setAnimalIssue(String animalIssue) {
+		this.animalIssue = animalIssue;
+	}
+
+	public Date getEntWantDate() {
+		return entWantDate;
+	}
+
+	public void setEntWantDate(Date entWantDate) {
+		this.entWantDate = entWantDate;
+	}
+
+	public int getEntWantTime() {
+		return entWantTime;
+	}
+
+	public void setEntWantTime(int entWantTime) {
+		this.entWantTime = entWantTime;
+	}
+
+	public String getReqPhone() {
+		return reqPhone;
+	}
+
+	public void setReqPhone(String reqPhone) {
+		this.reqPhone = reqPhone;
+	}
+
+	public Date getEntDate() {
+		return entDate;
+	}
+
+	public void setEntDate(Date entDate) {
+		this.entDate = entDate;
+	}
+
+	public String getAnimalType() {
+		return animalType;
+	}
+
+	public void setAnimalType(String animalType) {
+		this.animalType = animalType;
+	}
+
+	@Override
+	public String toString() {
+		return "Entrance [entNo=" + entNo + ", memNo=" + memNo + ", memName=" + memName + ", entTitle=" + entTitle
+				+ ", entApplyDate=" + entApplyDate + ", processResult=" + processResult + ", animalVariety="
+				+ animalVariety + ", animalName=" + animalName + ", animalGender=" + animalGender + ", animalAge="
+				+ animalAge + ", animalVaccinated=" + animalVaccinated + ", animalNeturalization="
+				+ animalNeturalization + ", animalDisease=" + animalDisease + ", animalIssue=" + animalIssue
+				+ ", entWantDate=" + entWantDate + ", entWantTime=" + entWantTime + ", reqPhone=" + reqPhone
+				+ ", entDate=" + entDate + ", animalType=" + animalType + "]";
+	}
+
+	
+	
 
 }
